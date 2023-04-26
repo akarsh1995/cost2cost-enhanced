30ab054cac7668525e03b939487d5996c23dc98e
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-04-26 12:53:50 +0530

    add metadata

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,6 +8,13 @@ async function getProductData() {
   return await res.json();
 }
 
+export const metadata = {
+  title: "COST TO COST PRICELIST PDF | with GST",
+  description:
+    "The only pricelist to build the best pc from Nehru Place COST TO COST. Calculate the price with GST to build",
+  keywords: ["cost to cost", "cost2cost", "pdf", "gst", "Nehru Place"],
+};
+
 const Home = async () => {
   const productData = await getProductData();
   return <Main productData={productData} />;
